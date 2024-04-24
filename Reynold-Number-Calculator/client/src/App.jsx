ace6e0f16d6c1e5abbf24b727a3f2fe05ab302ee
ibobey <ibrahim-tunc-02@hotmail.com>
2024-04-24 09:44:28 +0300

    initial commit 2

--- Reynold-Number-Calculator/client/src/App.jsx
+++ Reynold-Number-Calculator/client/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+import Header from "./components/header/Header.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
+import Home from "./pages/home/Home.jsx";
+import NotFound from "./pages/notfound/NotFound.jsx";
+
 
 function App() {
   return (
-    <>
-      <h1>HEllo</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
